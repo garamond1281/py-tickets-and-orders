bef187bfd73f16e0919c60cf27ce61209d4aba90
garamond128 <fenixlandit@icloud.com>
2026-09-16 15:03:42 +0300

    Fix

--- db/models.py
+++ db/models.py
@@ -102,7 +102,8 @@ class Ticket(models.Model):
                 {
                     "seat": "seat number must be "
                             "in available range: (1, seats_in_row): "
-                            f"(1, {self.movie_session.cinema_hall.seats_in_row})"
+                            f"(1, "
+                            f"{self.movie_session.cinema_hall.seats_in_row})"
                 }
             )
 
